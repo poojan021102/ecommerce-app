0798a5fbac96a5b4826ac3bfc3799783c7d86c6d
Poojan Patel <poojanpatel02112002@gmail.com>
2023-06-27 11:17:08 +0530

    bug fixes

--- src/pages/Completion.jsx
+++ src/pages/Completion.jsx
@@ -1,5 +1,19 @@
+import { checkIfUserAlreadyExists } from '../redux/actions';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 function Completion(props) {
-    return <h1>Thank you! 🎉</h1>;
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(checkIfUserAlreadyExists());
+  },[]);
+    return(
+      <div>
+        <h1>Thank you! 🎉</h1>
+        <div style = {{height:"140px"}}>
+
+        </div>
+      </div>
+    ) 
   }
   
   export default Completion;
